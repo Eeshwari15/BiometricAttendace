baf7e4f92c2eeb6ffc56221fa35d4e87f1c3cc89
Eeshwari <ebhatore@gmail.com>
2020-01-23 12:00:41 +0530

    Signed-off-by: Eeshwari <ebhatore@gmail.com>

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask
- 
+port = int(os.environ.get('PORT', 5000))
+
 app = Flask(__name__)
  
-@app.route('/helloesp')
+@app.route('/')
 def helloHandler():
     return 'Hello ESP8266, from Flask'
  
 
 if __name__=='__main__':
-	app.run(debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
+
